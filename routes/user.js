341fe16c4d6edfa7bea6dce7e0e6fb79b4c729ff
Kittiphop Vichitkijja <kittiphopvichitkijja@Kittiphops-MacBook-Pro.local>
2020-12-03 20:30:30 +0700

    Adding authorization

--- routes/user.js
+++ routes/user.js
@@ -12,9 +12,11 @@ router.post('/register', async (req, res) => {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
-        console.log(registeredUser)
-        req.flash('success', 'Welcome to YelpCamp')
-        res.redirect('/campgrounds');
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Welcome to YelpCamp')
+            res.redirect('/campgrounds');
+        })
     } catch (e) {
         req.flash('error', e.message);
         res.redirect('/register')
@@ -26,7 +28,15 @@ router.get('/login', (req, res) => {
 })
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     req.flash('success', 'Welcome back!')
-    res.redirect('/campgrounds')
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
+})
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success', 'Goodbye!');
+    res.redirect('/campgrounds');
 })
 
 module.exports = router;
