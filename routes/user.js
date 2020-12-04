f2b424069105f3daa2a7fb4c7f764398e21d7201
Kittiphop Vichitkijja <kittiphopvichitkijja@Kittiphops-MacBook-Pro.local>
2020-12-04 18:46:28 +0700

    Adding Controllers & Star Ratings

--- routes/user.js
+++ routes/user.js
@@ -1,42 +1,17 @@
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
-const User = require('../models/user');
+const user = require('../controllers/user');
 
-router.get('/register', (req, res) => {
-    res.render('user/register');
-})
-router.post('/register', async (req, res) => {
-    try {
 
-        const { email, username, password } = req.body;
-        const user = new User({ email, username });
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, err => {
-            if (err) return next(err);
-            req.flash('success', 'Welcome to YelpCamp')
-            res.redirect('/campgrounds');
-        })
-    } catch (e) {
-        req.flash('error', e.message);
-        res.redirect('/register')
-    }
-})
+router.route('/register')
+    .get(user.renderRegister)
+    .post(user.register)
 
-router.get('/login', (req, res) => {
-    res.render('user/login')
-})
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-    req.flash('success', 'Welcome back!')
-    const redirectUrl = req.session.returnTo || '/campgrounds';
-    delete req.session.returnTo;
-    res.redirect(redirectUrl);
-})
+router.route('/login')
+    .get(user.renderLogin)
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), user.login)
 
-router.get('/logout', (req, res) => {
-    req.logout();
-    req.flash('success', 'Goodbye!');
-    res.redirect('/campgrounds');
-})
+router.get('/logout', user.logout)
 
 module.exports = router;
